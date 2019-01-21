88156a4ebb0b4c3c60b18435749e38388fa94965
Joe Wu <joe@joecwu.com>
2019-01-22 01:23:12 +0800

    DateTime support JST

--- es-analyzer-compare-web/src/components/DateTime.js
+++ es-analyzer-compare-web/src/components/DateTime.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 var PropTypes = require('prop-types');
 
-/*jshint ignore:start*/
-const DateTime = ({ value }) => <span>{(typeof value === 'undefined') ? '' : value.toLocaleString()}</span>;
-/*jshint ignore:end*/
+class DateTime extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const value = this.props.value;
+    if(this.props.timeZone == 'Asia/Tokyo') {
+      return (<span>{(typeof value === 'undefined') ? '' : value.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })} (JST)</span>);
+    } else {
+      return (<span>{(typeof value === 'undefined') ? '' : value.toLocaleString()}</span>);
+    }
+  }
+}
 
 DateTime.propTypes = {
-  value: PropTypes.instanceOf(Date)
+  value: PropTypes.instanceOf(Date),
+  timeZone: PropTypes.string
 };
 
 export default DateTime;
