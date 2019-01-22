ce07d103a2fe12619006acfceb888f0eea942d8c
Joe Wu <joe@joecwu.com>
2019-01-22 20:49:58 +0800

    add checkbox list to control comparison items

--- es-analyzer-compare-web/src/containers/App.js
+++ es-analyzer-compare-web/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Grid, Row, Col, Panel, FormGroup, FormControl, Button, Form, Tabs, Tab } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, FormGroup, FormControl, Button, Form, Tabs, Tab, Checkbox } from 'react-bootstrap';
 import SearchRespList from '../components/SearchRespList';
 
 class App extends Component {
@@ -11,7 +11,17 @@ class App extends Component {
       key: 1,
       keyword: '',
       tempKeyword: '',
-      pageSize: '30'
+      pageSize: '30',
+      song_cbx_01: true,
+      song_cbx_02: true,
+      song_cbx_03: true,
+      song_cbx_04: true,
+      song_cbx_05: true,
+      song_cbx_06: true,
+      song_cbx_07: false,
+      song_cbx_08: false,
+      song_cbx_09: false,
+      song_cbx_10: false,
     };
     this.search = this.search.bind(this);
     this.keywordChange = this.keywordChange.bind(this);
@@ -19,6 +29,7 @@ class App extends Component {
     this.refreshSong = this.refreshSong.bind(this);
     this.refreshSongByArtist = this.refreshSongByArtist.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   keywordChange(event) {
@@ -30,11 +41,11 @@ class App extends Component {
     console.log('search for keyword:' + this.state.tempKeyword);
     this.setState({ keyword: this.state.tempKeyword }, () => {
       console.log('tab:' + this.state.key);
-      if(this.state.key == 1) {
+      if (this.state.key == 1) {
         this.refreshSong();
-      }else if(this.state.key == 2) {
+      } else if (this.state.key == 2) {
         this.refreshSongByArtist();
-      }else if(this.state.key == 3) {
+      } else if (this.state.key == 3) {
         this.refreshPlaylist();
       }
     });
@@ -52,6 +63,12 @@ class App extends Component {
 
   handleSelect(key) {
     this.setState({ key });
+  }
+
+  handleChange(evt) {
+    console.log(evt);
+    console.log(evt.target.id);
+    this.setState({ [evt.target.id]: evt.target.checked });
   }
 
   /* jshint ignore:start */
@@ -76,11 +93,29 @@ class App extends Component {
             </Button>
           </FormGroup>
         </Form>
-        <Tabs defaultActiveKey={1} animation={false} activeKey={this.state.key} onSelect={this.handleSelect} id="noanim-tab-example">
+        <Tabs
+          defaultActiveKey={1}
+          animation={false}
+          activeKey={this.state.key}
+          onSelect={this.handleSelect}
+          id="noanim-tab-example"
+        >
           <Tab eventKey={1} title="Song">
-          <Grid style={{ margin: '0px', width: '100%' }}>
+          <p>
+            <Checkbox id="song_cbx_01" checked={this.state['song_cbx_01']} onChange={this.handleChange} inline >1. Original</Checkbox>
+            <Checkbox id="song_cbx_02" checked={this.state['song_cbx_02']} onChange={this.handleChange} inline >2. Original (人気順)</Checkbox>
+            <Checkbox id="song_cbx_03" checked={this.state['song_cbx_03']} onChange={this.handleChange} inline >3. Original (新着順)</Checkbox>
+            <Checkbox id="song_cbx_04" checked={this.state['song_cbx_04']} onChange={this.handleChange} inline >4. Readingform</Checkbox>
+            <Checkbox id="song_cbx_05" checked={this.state['song_cbx_05']} onChange={this.handleChange} inline >5. Readingform (score only)</Checkbox>
+            <Checkbox id="song_cbx_06" checked={this.state['song_cbx_06']} onChange={this.handleChange} inline >6. Readingform (phrase^3)</Checkbox>
+            <Checkbox id="song_cbx_07" checked={this.state['song_cbx_07']} onChange={this.handleChange} inline >7. Readingform (phrase^3, 人気順)</Checkbox>
+            <Checkbox id="song_cbx_08" checked={this.state['song_cbx_08']} onChange={this.handleChange} inline >8. Readingform (phrase^3, 新着順)</Checkbox>
+            <Checkbox id="song_cbx_09" checked={this.state['song_cbx_09']} onChange={this.handleChange} inline >9. Readingform Song+Artist (phrase^3)</Checkbox>
+            <Checkbox id="song_cbx_10" checked={this.state['song_cbx_10']} onChange={this.handleChange} inline >10. Readingform Song+Artist (score only)</Checkbox>
+          </p>
+            <Grid style={{ margin: '0px', width: '100%' }}>
               <Row className="show-grid" style={{ width: '100%' }}>
-                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_01 ? 'inline' : 'none' }}>
                   <SearchRespList
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
@@ -90,7 +125,7 @@ class App extends Component {
                     pageSize={this.state.pageSize}
                   />
                 </Col>
-                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_02 ? 'inline' : 'none' }}>
                   <SearchRespList
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
@@ -100,7 +135,7 @@ class App extends Component {
                     pageSize={this.state.pageSize}
                   />
                 </Col>
-                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_03 ? 'inline' : 'none' }}>
                   <SearchRespList
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
@@ -110,7 +145,7 @@ class App extends Component {
                     pageSize={this.state.pageSize}
                   />
                 </Col>
-                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_04 ? 'inline' : 'none' }}>
                   <SearchRespList
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
@@ -120,22 +155,62 @@ class App extends Component {
                     pageSize={this.state.pageSize}
                   />
                 </Col>
-                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_05 ? 'inline' : 'none' }}>
                   <SearchRespList
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
                     searchTemplate="song_v2_readingform_score"
-                    title="4. Readingform (score only)"
+                    title="5. Readingform (score only)"
                     refresh={this.state.refreshSong}
                     pageSize={this.state.pageSize}
                   />
                 </Col>
-                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_06 ? 'inline' : 'none' }}>
                   <SearchRespList
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
                     searchTemplate="song_v2_readingform_boost_keyword"
-                    title="5. Readingform (Keyword^10)"
+                    title="6. Readingform (phrase^3)"
+                    refresh={this.state.refreshSong}
+                    pageSize={this.state.pageSize}
+                  />
+                </Col>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_07 ? 'inline' : 'none' }}>
+                  <SearchRespList
+                    keyword={this.state.keyword}
+                    searchIndex="utapass_song_20181001_v3"
+                    searchTemplate="song_v2_readingform_boost_keyword_popularity"
+                    title="7. Readingform (phrase^3, 人気順)"
+                    refresh={this.state.refreshSong}
+                    pageSize={this.state.pageSize}
+                  />
+                </Col>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_08 ? 'inline' : 'none' }}>
+                  <SearchRespList
+                    keyword={this.state.keyword}
+                    searchIndex="utapass_song_20181001_v3"
+                    searchTemplate="song_v2_readingform_boost_keyword_releasedate"
+                    title="8. Readingform (phrase^3, 新着順)"
+                    refresh={this.state.refreshSong}
+                    pageSize={this.state.pageSize}
+                  />
+                </Col>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_09 ? 'inline' : 'none' }}>
+                  <SearchRespList
+                    keyword={this.state.keyword}
+                    searchIndex="utapass_song_20181001_v3"
+                    searchTemplate="song_v2_readingform_all_boost_keyword"
+                    title="9. Readingform Song+Artist (phrase^3)"
+                    refresh={this.state.refreshSong}
+                    pageSize={this.state.pageSize}
+                  />
+                </Col>
+                <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px', display: this.state.song_cbx_10 ? 'inline' : 'none' }}>
+                  <SearchRespList
+                    keyword={this.state.keyword}
+                    searchIndex="utapass_song_20181001_v3"
+                    searchTemplate="song_v2_all_readingform_score"
+                    title="10. Readingform Song+Artist (score only)"
                     refresh={this.state.refreshSong}
                     pageSize={this.state.pageSize}
                   />
@@ -144,7 +219,7 @@ class App extends Component {
             </Grid>
           </Tab>
           <Tab eventKey={2} title="Song by Artist">
-          <Grid style={{ margin: '0px', width: '100%' }}>
+            <Grid style={{ margin: '0px', width: '100%' }}>
               <Row className="show-grid" style={{ width: '100%' }}>
                 <Col lg={2} md={3} sm={4} style={{ margin: '0px', padding: '1px' }}>
                   <SearchRespList
@@ -191,7 +266,7 @@ class App extends Component {
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
                     searchTemplate="song_by_artist_v2_readingform_score"
-                    title="4. Readingform (score only)"
+                    title="5. Readingform (score only)"
                     refresh={this.state.refreshSongByArtist}
                     pageSize={this.state.pageSize}
                   />
@@ -201,7 +276,7 @@ class App extends Component {
                     keyword={this.state.keyword}
                     searchIndex="utapass_song_20181001_v3"
                     searchTemplate="song_by_artist_v2_readingform_boost_keyword"
-                    title="5. Readingform (Keyword^10)"
+                    title="6. Readingform (phrase^3)"
                     refresh={this.state.refreshSongByArtist}
                     pageSize={this.state.pageSize}
                   />
